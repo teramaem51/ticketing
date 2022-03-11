f397c5e58f8f3ba1efa74404bef6701b0f5df938
teramaem51 <teramaem51@gmail.com>
2022-03-11 22:54:15 +0900

    A Reusable API Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 
 const LandingPage = ({ currentUser }) => {
@@ -9,25 +9,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  // console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // we are on the server
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        'headers': req.headers
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    // we are on the browser
-    
-    return data;
-  }
-  return {};
+  return data;
 };
 
 export default LandingPage;
