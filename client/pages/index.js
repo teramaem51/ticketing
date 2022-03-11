088e74d01ae3b4c25e3b20a76cbd8332403a6f00
teramaem51 <teramaem51@gmail.com>
2022-03-11 16:28:42 +0900

    Fetching Data During SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const index = () => {
+import axios from 'axios';
+
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  // console.log(req.headers);
+  if (typeof window === 'undefined') {
+    // we are on the server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        'headers': req.headers
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    // we are on the browser
+    
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
