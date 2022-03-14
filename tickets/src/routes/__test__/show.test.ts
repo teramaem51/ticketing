3e0de46ff6c31b3b23363986ef8497573dca29de
teramaem51 <teramaem51@gmail.com>
2022-03-15 06:41:56 +0900

    update ticket show feature

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,29 +1,33 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 
 it('returns a 404 if the ticket is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
   await request(app)
-    .get('/api/tickets/lkdjfuegsvdljfsllk')
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 });
 
 it('returns the ticket if the ticket is found', async () => {
   const title = 'concert';
-  const price = 20
+  const price = 20;
   
   const response = await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({
-      title, price
+      title,
+      price
     })
     .expect(201);
   
   const ticketResponce = await request(app)
     .get(`/api/tickets/${response.body.id}`)
+    .send()
     .expect(200);
   
   expect(ticketResponce.body.title).toEqual(title);
