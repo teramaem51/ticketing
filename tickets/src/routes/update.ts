51f1b6448ac729d5a73bc60529a00935571ecafc
teramaem51 <teramaem51@gmail.com>
2022-03-16 08:54:05 +0900

    add title and price validation test

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -11,18 +11,29 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
-  const ticket = await Ticket.findById(req.params.id);
+router.put('/api/tickets/:id', requireAuth,
+  [
+    body('title')
+      .not()
+      .isEmpty()
+      .withMessage('Title is required'),
+    body('price')
+      .isFloat({ 'gt': 0})
+      .withMessage('Price must be provided and must be greater than 0')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
 
-  if (!ticket) {
-    throw new NotFoundError();
-  }
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
-  if (ticket.userId !== req.currentUser!.id) {
-    throw new NotAuthorizedError();
-  }
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
 
-  res.send(ticket);
+    res.send(ticket);
 });
 
 export { router as updateTicketRouter };
