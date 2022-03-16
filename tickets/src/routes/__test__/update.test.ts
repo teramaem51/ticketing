4edb02a5728e52a0b89ff682d01e960fedb07480
teramaem51 <teramaem51@gmail.com>
2022-03-16 09:05:17 +0900

    add ticket update test

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -75,6 +75,30 @@ it('returns a 400 if the user provides an invalid title or price', async () => {
     .expect(400);
 });
 
-it('updates the ticket provided valid inputs', async () => {});
+it('updates the ticket provided valid inputs', async () => {
+  const cookie = global.signin();
 
-it('', async () => {});
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      'title': 'exampletitle',
+      'price': 20
+    });
+  
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      'title': 'new title',
+      'price': 100
+    })
+    .expect(200);
+  
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+  
+  expect(ticketResponse.body.title).toEqual('new title');
+  expect(ticketResponse.body.price).toEqual(100);
+});
