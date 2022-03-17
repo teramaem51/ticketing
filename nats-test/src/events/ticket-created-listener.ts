4cdafe8af8107096c6d292cb79e9e42dd333a918
teramaem51 <teramaem51@gmail.com>
2022-03-17 14:50:33 +0900

    add subject and event interfaces

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,13 +1,18 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-events';
+import { Subjects } from './subjects';
 
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: any, msg: Message) {
     console.log('Event data!', data);
+
+    console.log(data.name);
+    console.log(data.cost);
 
     msg.ack();
   }
