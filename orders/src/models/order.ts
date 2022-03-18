cb927cea75267ed05d1202a012c949ae61206eb3
teramaem51 <teramaem51@gmail.com>
2022-03-18 19:15:06 +0900

    add finding ticket and is-reserved logic to orders service

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@mt51tickets/common';
+import { TicketDoc } from './ticket';
 
+
+export { OrderStatus };
 
 interface OrderAttrs {
   userId: string;
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@mt51tickets/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@mt51tickets/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 
 const router = express.Router();
@@ -16,6 +18,29 @@ router.post('/api/orders', requireAuth,
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // if we find an order form that means the ticket *is* reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+    
+    // Build the order and save it to the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
