4e657b8d2174a199d005c8081afde4e502746837
teramaem51 <teramaem51@gmail.com>
2022-03-24 01:45:46 +0900

    add 10 seconds test case expiration

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,7 +10,8 @@ import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+// const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 10;
 
 router.post('/api/orders', requireAuth,
   [
