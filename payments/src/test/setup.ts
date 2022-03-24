5518f64a91ef8e10308ed528a5decf0381f66c7b
teramaem51 <teramaem51@gmail.com>
2022-03-24 14:04:49 +0900

    add test for new payment route in payments service

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -35,10 +35,10 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Build a JWT payload. { id, email }
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com'
   }
 
