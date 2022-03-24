e7a5dfbfade6ed02a00bf66b4b0bd38a13c6ba83
teramaem51 <teramaem51@gmail.com>
2022-03-24 13:26:48 +0900

    add order validation for new payment request

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
   requireAuth,
   validateRequest
 } from '@mt51tickets/common';
@@ -21,7 +25,22 @@ router.post('/api/payments',
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
     res.send({ 'success': true });
-});
+  }
+);
 
 export { router as createChargeRouter };
