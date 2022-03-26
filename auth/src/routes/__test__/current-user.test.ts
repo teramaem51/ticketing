072756604564b620241dfdeeedc55c9e6bdb3200
teramaem51 <teramaem51@gmail.com>
2022-03-26 17:23:22 +0900

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,24 +2,14 @@ import request from 'supertest';
 import { app } from '../../app';
 
 
-it('responds with details about the current user', async () => {
-  // const authResponse = await request(app)
-  //   .post('/api/users/signup')
-  //   .send({
-  //     email: 'test@test.com',
-  //     password: 'password'
-  //   })
-  //   .expect(201);
-  
-  // const cookie = authResponse.get('Set-Cookie');
-  
+it('responds with details about the current user', async () => {  
   const cookie = await global.signin();
 
   const responce = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send({})
-    .expect(200);
+    .expect(400);
   
   expect(responce.body.currentUser.email).toEqual('test@test.com')
 });
